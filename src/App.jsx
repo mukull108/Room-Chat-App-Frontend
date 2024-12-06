23b416e2bbcc0f0cf937c7958d7a99a802b2f6cf
mukullFromMac <mukullsavita@mac.com>
2024-12-06 15:45:03 +0530

    moved back to run on locally

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import toast from 'react-hot-toast'
 import JoinChat from './components/JoinChat'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
