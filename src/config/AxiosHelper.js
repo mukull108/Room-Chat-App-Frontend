bc4a6c45ca9bf577e7cba1e3a8356616ad080dc4
mukullFromMac <mukullsavita@mac.com>
2024-12-06 15:22:06 +0530

    edited connecting urls

--- src/config/AxiosHelper.js
+++ src/config/AxiosHelper.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseURL= "room-chat-app-backend-production.up.railway.app";
+export const baseURL= "https://room-chat-app-backend-production.up.railway.app";
 export const httpClient=axios.create({
     baseURL:baseURL,
 });
