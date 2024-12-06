b9a4730367fe4f47e646148d7c1bca74ce913422
mukullFromMac <mukullsavita@mac.com>
2024-12-06 14:45:40 +0530

    changed urls for communication to the backend

--- src/config/AxiosHelper.js
+++ src/config/AxiosHelper.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseURL= "http://localhost:8080";
+export const baseURL= "room-chat-app-backend.railway.internal";
 export const httpClient=axios.create({
     baseURL:baseURL,
 });
